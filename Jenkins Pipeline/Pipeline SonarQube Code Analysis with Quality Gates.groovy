69ad472fab283671b7b8ccbad01b29a6773646fa
Bharath Adigopula <153508760+adigopulabharath@users.noreply.github.com>
2024-03-21 12:18:34 +0530

    Add files via upload

---
+++ Jenkins Pipeline/Pipeline SonarQube Code Analysis with Quality Gates.groovy
@@ -0,0 +1,75 @@
+pipeline {
+	agent any
+	tools {
+		// Tools configuration
+	    maven "MavenV3"
+	    jdk "JavaJDK11"
+	}
+
+	stages {
+	    stage('Fetch code') {
+			// Fetch code from VCM (GitHub)
+            steps {
+               git branch: 'main', url: 'https://github.com/adigopulabharath/vprofile-project.git'
+            }
+
+	    }
+
+	    stage('Build'){
+			// Build
+	        steps{
+	           sh 'mvn clean install -DskipTests'
+	        }
+
+	        post {
+				// Post build actions, Archive, Notifications
+	           success {
+	              echo 'Now Archiving it...'
+	              archiveArtifacts artifacts: '**/target/*.war'
+	           }
+	        }
+	    }
+
+	    stage('Unit Test') {
+			// Performing unit test
+            steps{
+                sh 'mvn test'
+            }
+        }
+
+		 stage('Checkstyle Analysis'){
+            steps {
+                sh 'mvn checkstyle:checkstyle'
+            }
+        }
+
+		stage('Sonar Analysis') {
+            environment {
+                SonarScannerHome = tool 'SonarScannerV4.7'
+            }
+            steps {
+               withSonarQubeEnv('SonarQube') {
+                   sh '''${SonarScannerHome}/bin/sonar-scanner -Dsonar.projectKey=vprofile \
+                   -Dsonar.projectName=vprofile \
+                   -Dsonar.projectVersion=1.0 \
+                   -Dsonar.sources=src/ \
+                   -Dsonar.java.binaries=target/test-classes/com/visualpathit/account/controllerTest/ \
+                   -Dsonar.junit.reportsPath=target/surefire-reports/ \
+                   -Dsonar.jacoco.reportsPath=target/jacoco.exec \
+                   -Dsonar.java.checkstyle.reportPaths=target/checkstyle-result.xml'''
+              }
+            }
+        }
+
+		stage("Quality Gate") {
+            steps {
+                timeout(time: 1, unit: 'HOURS') {
+                    // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
+                    // true = set pipeline to UNSTABLE, false = don't
+                    waitForQualityGate abortPipeline: true
+                }
+            }   
+        }
+	}
+    
+}
